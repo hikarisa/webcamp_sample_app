0e26bc52932ee6b138e55787d03f3476c2454d68
EC2 Default User <ec2-user@ip-172-31-27-234.ec2.internal>
2022-04-23 05:26:56 +0000

    [fin]4-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,16 @@
 class ListsController < ApplicationController
   def new
+    # Viewへ渡すためのインスタンス変数にからのModelオブジェクトを生成する。
+    @list = List.new
+  end
+  
+  def create
+    # データの受け取りと新規登録
+    list = List.new(list_params)
+    # データベースに保存
+    list.save
+    # トップ画面へリダイレクト
+    redirect_to '/top'
   end
 
   def index
@@ -9,5 +20,11 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
